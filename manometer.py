f33371097896b12f1ab00be365341029c7023b9e
Kevin Meyer <meyerkevinstefan@gmail.com>
2024-03-04 15:20:27 +0100

    Update API.

--- manometer.py
+++ manometer.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 st.title("Meyer Hotel")
 
-client = OpenAI(api_key="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")
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
 if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-3.5-turbo"
